46a09660ad86587358d8583e08c14e9dac16aacd
Nikolai Golub <nikolay.v.golub@gmail.com>
2020-05-02 11:41:16 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// use gpio_cdev::{Chip, LineRequestFlags};
+
+fn main() {
+    println!("yo");
+    // let inputline = 3;
+    // let mut chip = Chip::new("/dev/gpiochip0")?;
+    // let input = chip.get_line(inputline)?;
+    println!("Done");
+}
