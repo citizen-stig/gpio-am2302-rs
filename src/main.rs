0e758c2a28bd140ef56e04b5dd72b2ace24585fc
Nikolai Golub <nikolay.v.golub@gmail.com>
2020-05-17 21:14:10 +0200

    More clean ups

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,21 @@ use am2302::Reading;
 
 
 fn try_read(gpio_number: u32) -> Option<Reading> {
+    let mut final_result = None;
     let all_data = push_pull(gpio_number);
     if all_data.len() < 40 {
         println!("Saad, read not enough data");
+        return final_result;
     }
-    let mut final_result = None;
     for data in all_data.windows(40) {
         let result = Reading::from_binary_vector(&data);
         match result {
             Ok(reading) => {
                 final_result = Some(reading);
                 break;
-
             }
-            Err(e) => { println!("Error: {:?}", e)
+            Err(e) => {
+                println!("Error: {:?}", e)
             }
         }
     }
@@ -29,7 +30,6 @@ fn try_read(gpio_number: u32) -> Option<Reading> {
 }
 
 fn main() {
-    // let gpio_number = 17;    // GPIO17 (11)
     let gpio_number = 4;  // GPIO4  (7)
     let sleep_time = time::Duration::from_secs(5);
     for _ in 1..30 {
@@ -40,8 +40,4 @@ fn main() {
             None => println!("Unable to get the data"),
         }
     }
-
-
-    // events_sub(gpio_number);
-    // read(gpio_number);
 }
